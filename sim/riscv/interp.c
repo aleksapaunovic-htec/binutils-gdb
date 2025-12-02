4eb0a3728ef06497dc6d387ead6066b09df68b09
Aleksa Paunovic <aleksa.paunovic@htecgroup.com>
2025-12-02 14:15:26 +0100

    gdb/sim/riscv: Fix default endianness
    The default byte order for RISC-V was assumed to be little-endian,
    which required passing -EB when running for big-endian targets.
    This makes it easier to use the existing testing infrastructure
    on big-endian builds.

--- sim/riscv/interp.c
+++ sim/riscv/interp.c
@@ -73,7 +73,7 @@ sim_open (SIM_OPEN_KIND kind, host_callback *callback,
   /* Set default options before parsing user options.  */
   STATE_MACHS (sd) = riscv_sim_machs;
   STATE_MODEL_NAME (sd) = WITH_TARGET_WORD_BITSIZE == 32 ? "RV32G" : "RV64G";
-  current_target_byte_order = BFD_ENDIAN_LITTLE;
+  current_target_byte_order = WITH_TARGET_BYTE_ORDER;
   callback->syscall_map = cb_riscv_syscall_map;
 
   /* The cpu data is kept in a separately allocated chunk of memory.  */
